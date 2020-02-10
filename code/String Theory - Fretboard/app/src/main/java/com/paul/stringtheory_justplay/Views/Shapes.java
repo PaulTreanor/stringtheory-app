bdbc19cec2969e086cff9c053ed93289bbec5e0e
Paul Treanor <treanorpaul9@gmail.com>
2020-02-10 19:17:12 +0000

    creating shapes for fretboard layout

--- code/String Theory - Fretboard/app/src/main/java/com/paul/stringtheory_justplay/Views/Shapes.java
+++ code/String Theory - Fretboard/app/src/main/java/com/paul/stringtheory_justplay/Views/Shapes.java
@@ -1,4 +1,73 @@
 package com.paul.stringtheory_justplay.Views;
 
-public class Shapes {
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+
+public class Shapes extends View {
+
+    public Shapes(Context context) {
+        super(context);
+
+        init(null);
+    }
+
+    public Shapes(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+
+        init(attrs);
+
+    }
+
+    public Shapes(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        init(attrs);
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public Shapes(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+
+        init(attrs);
+
+    }
+
+    private void init(@Nullable AttributeSet set) {
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas){
+
+        //create rectangle
+        Rect rect = new Rect();
+        rect.top = 140;
+        rect.left = 1820;
+        rect.bottom = rect.top + 800;
+        rect.right = rect.left + 100;
+
+        //define circle parameters
+        float cx = 0;
+        float cy = 540;
+        float radius = 400f;
+
+        //define paint object to colour shapes
+        @SuppressLint("DrawAllocation") Paint paint = new Paint();
+        paint.setColor(Color.GREEN);
+
+        //draw circle and rectangle
+        canvas.drawCircle(cx, cy, radius, paint);
+        canvas.drawRect(rect, paint);
+    }
 }
