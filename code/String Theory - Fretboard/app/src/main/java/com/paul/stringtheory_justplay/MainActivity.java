f02f2f001303cc982a896da35c6f010ae2b39c2f
Paul Treanor <treanorpaul9@gmail.com>
2020-02-15 15:31:45 +0000

    got app to play a midi file from res

--- code/String Theory - Fretboard/app/src/main/java/com/paul/stringtheory_justplay/MainActivity.java
+++ code/String Theory - Fretboard/app/src/main/java/com/paul/stringtheory_justplay/MainActivity.java
@@ -2,12 +2,17 @@ package com.paul.stringtheory_justplay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.JetPlayer;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer player;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +28,43 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public static void playf(View view){
+    public void playf(View v){
         //int[] pitchArray = {JMC.A5, JMC.D5, JMC.F5};
         //Chords newChord = new Chords(pitchArray);
-
+        if (player ==null){
+            player = MediaPlayer.create(this, R.raw.chords);
+            player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    stopPlayer();
+                }
+            });
+        }
+        player.start();
 
     }
+    public void pause(View v){
+        if (player  !=null){
+            player.pause();
+        }
+
     }
+    public void stop(View v){
+
+        stopPlayer();
+    }
+
+    private void stopPlayer(){
+        if (player !=null){
+            player.release();
+            player = null;
+        }
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+        stopPlayer();
+
+    }
+}
 
